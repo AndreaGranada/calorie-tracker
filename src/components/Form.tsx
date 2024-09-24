90ec98f6404f36fa8ff741bdbd87f92cce172862
AndreaGranada <andreaalejandre93@gmail.com>
2024-09-24 20:19:54 +0200

    types

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,21 @@
 import { categories } from "../data/categories"
 import { useState, ChangeEvent } from "react"
+import { Activity } from "../types"
 
 export default function Form() {
 
-    const [activity, setActivity] = useState({
+    const [activity, setActivity] = useState<Activity>({
         category: 1,
         name: '',
         calories: 0
     })
 
     const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+        const isNumberField = ['category', 'calories'].includes(e.target.id)
+        console.log(isNumberField)
         setActivity({
             ...activity,
-            [e.target.id] : e.target.value
+            [e.target.id] : isNumberField ? +e.target.value : e.target.value
         })
         
     }
